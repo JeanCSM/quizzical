4910c5c75e6364c95b9db4a6cb146f85a18ccbc9
Jonathan Wilde <speedbreeze@gmail.com>
2010-07-20 22:20:24 -0700

    Adding the Powers class, which manages group permissions within the application.

---
+++ source/system/application/libraries/Powers.php
@@ -0,0 +1,92 @@
+<?php if (!defined('BASEPATH')) exit('Direct access not allowed.');
+
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Quizzical.
+ *
+ * The Initial Developer of the Original Code is Jonathan Wilde.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+class Powers {
+
+	var $my_powers = false;
+	var $group = false;
+	var $user = false;
+	var $powers = array();
+	var $CI;
+
+	public function __construct () {
+		// Load a reference to our instance of CodeIgniter
+		$this->CI =& get_instance();
+
+		// Load the power model
+		$this->CI->load->model("powers_model");
+
+		// Grab the current user
+        $id_column = $this->CI->config->item("identity");
+        $this->user = $this->CI->session->userdata($id_column);
+
+		// Check to see whether the user is logged in or not
+        if ($this->user != false) {
+            // If so, load information about the user from the database
+            $this->group = $this->CI->powers_model->get_group_id($id_column, $this->user);
+
+            // Load information about the user's group's powers
+            $this->my_powers = $this->CI->powers_model->get_group_powers($this->group);
+        } else {
+            // Load information about the powers that users that aren't
+            // logged in have
+            $this->my_powers = $this->CI->powers_model->get_group_powers(-1);
+        }
+	}
+
+	public function i_can ($power, $redirect = false) {
+		// Determine if the user has the specified power
+		$can = array_search($power, $this->my_powers) !== false;
+
+		// If we're supposed to redirect the user to the login page if
+		// they're not logged in, do so
+		if (!$this->user && $redirect == true)
+			redirect("accounts/login");
+
+		// Return whether the user has the specified power
+		return $can;
+	}
+
+	public function register ($power) {
+		// If the powers are provided as an array, merge them in;
+		// otherwise, push the power onto the end of the array
+		if (is_array($power))
+			$this->powers = array_merge($this->powers, $power);
+		else
+			array_push($this->powers, $power);
+	}
+}
