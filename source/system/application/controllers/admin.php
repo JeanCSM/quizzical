465ecb29b65407e0d92df634f222ddf0258097e3
Jonathan Wilde <speedbreeze@gmail.com>
2010-07-25 23:44:51 -0700

    Added a quiz handler to the Admin class, which manages the quiz creation, editing, and deleting forms and validation.

--- source/system/application/controllers/admin.php
+++ source/system/application/controllers/admin.php
@@ -62,4 +62,72 @@ class Admin extends MY_Controller {
 		$this->dwootemplate->display('admin/quizzes.tpl');
 	}
 	
+	function quiz ($action) {
+		$this->load->model('Quizzes_model');
+		$this->load->model('Questions_model');
+		$this->load->library('form_validation');
+		
+		// Try to determine the id of the quiz being edited/deleted
+		$id = $this->uri->segment(4);
+		
+		// If the quiz is being created or edited, add validation rules; those
+		// two actions use the same form and thus use the same form validation
+		// rules in the same way
+		if ($action == 'update' || $action == 'edit') {
+			$this->form_validation->add_rules('title', 'Title', 'required');
+			$this->form_validation->add_rules('tries', 'Max Tries', 'integer');
+		}
+		
+		// Make sure to set the selected section of the admin subnavigation to
+		// the "Quizzes" section
+		$this->dwootemplate->assign('selected_section', 'quizzes');
+		
+		switch ($action) {
+			case 'update':
+				// Retrieve existing data regarding the quiz
+				$this->dwootemplate->assign('quiz',
+					$this->Quizzes_model->get_where_id($id));
+				$this->dwootemplate->assign('questions',
+					$this->Questions_model->get_where_quiz($id));
+				
+				// If validation passed, push the changes into the database
+				if ($this->form_validation->run()) {
+					$this->Quizzes_model->update(
+						$id,
+						$input->type->post('title', true),
+						$input->type->post('summary', true),
+						$input->type->post('published', true),
+						$input->type->post('tries', true)
+					);
+				}
+				
+				// Display the quiz editing form so the user can make changes
+				$this->dwootemplate->assign('action', 'edit');
+				$this->dwootemplate->display('admin/quiz.tpl');
+				
+				break;
+			case 'create':
+				if ($this->form_validation->run()) {
+					// If validation passed, push the new quiz into the database
+					// and redirect the user over to a page to continue adding
+					// questions to that quiz
+					$this->Quizzes_model->create(
+						$input->type->post('title', true),
+						$input->type->post('summary', true),
+						$input->type->post('published', true),
+						$input->type->post('tries', true)
+					);
+					
+					redirect('admin/quiz/edit/'. $this->db->insert_id());
+					return;
+				} else {
+					// Since nothing was submitted, display an empty form that
+					// allows a user to create a new quiz
+					$this->dwootemplate->assign('action', 'create');
+					$this->dwootemplate->display('admin/quiz.tpl');
+				}
+				break;
+		}
+	}
+	
 }
