887b8b0ed75c63a7fb5a5b9850a2707ab67d0a63
Jonathan Wilde <hello@jwilde.me>
2012-09-16 03:16:51 -0400

    More m/r tools.

--- jobs/cleanup.py
+++ jobs/cleanup.py
@@ -13,3 +13,9 @@ def delete(entity):
     if entity.quiz.key().id() == quiz_id:
         entity.is_archived = True
         yield operation.db.Delete(entity)
+
+def dump(entity):
+    yield operation.db.Delete(entity)
+
+def poke(entity):
+    yield operation.db.Put(entity)
